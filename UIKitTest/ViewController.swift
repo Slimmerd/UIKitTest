e9295d2b5a7d7715fe70bdae588fb9e068e963a2
Daniil Silin <daniil_silin@mail.ru>
2022-03-21 23:55:16 +0000

    Basic button and alert

--- UIKitTest/ViewController.swift
+++ UIKitTest/ViewController.swift
@@ -14,6 +14,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Error", message: "Bob!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "kok", style: .default) { action in
+            let text = alertController.textFields?.first?.text
+            print(text ?? "nil")
+        }
+        
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+//    MARK: - Better implementation
+    
+    func alert(title: String, message: String, style: UIAlertController.Style) {
+        
+    }
+    
 }
 
