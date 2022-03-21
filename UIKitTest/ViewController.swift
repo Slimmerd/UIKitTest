1a8c48e8a128f36a4f4aaf83215bc4c8a73d7d8b
Daniil Silin <daniil_silin@mail.ru>
2022-03-21 23:27:10 +0000

    Initial Commit

---
+++ UIKitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTest
+//
+//  Created by Daniil Silin on 21/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
