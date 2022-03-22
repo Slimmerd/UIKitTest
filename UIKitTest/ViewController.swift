84d7d68dac93e1444ded3db5c857be4d9bbb4f9f
Daniil Silin <daniil_silin@mail.ru>
2022-03-22 00:05:15 +0000

    Better UIButton implementation and using additional labels

--- UIKitTest/ViewController.swift
+++ UIKitTest/ViewController.swift
@@ -9,28 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showButton(_ sender: Any) {
-        let alertController = UIAlertController(title: "Error", message: "Bob!", preferredStyle: .alert)
-        let action = UIAlertAction(title: "kok", style: .default) { action in
-            let text = alertController.textFields?.first?.text
-            print(text ?? "nil")
-        }
         
-        alertController.addTextField(configurationHandler: nil)
-        alertController.addAction(action)
-        self.present(alertController, animated: true, completion: nil)
-        
+        self.welcomeLabel.text = "Hi,"
+        self.alert(title: "Who are you", message: "Huh?", style: .alert)
     }
     
 //    MARK: - Better implementation
     
     func alert(title: String, message: String, style: UIAlertController.Style) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "kok", style: .default) { action in
+            let text = alertController.textFields?.first?.text
+            self.welcomeLabel.text! += text!
+        }
         
+        alertController.addTextField { textField in
+            textField.isSecureTextEntry = true
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
     }
     
 }
