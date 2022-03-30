0cd1f7e8b9ff57690fa212899965233489feea19
Daniil Silin <daniil_silin@mail.ru>
2022-03-30 01:42:11 +0100

    Advanced TextField usage

--- UIKitTest/ViewController.swift
+++ UIKitTest/ViewController.swift
@@ -7,35 +7,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
 
-    let myLabel = UILabel()
+    var myTextField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.createTextField()
         
-        let labelFrame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        self.myTextField.delegate = self
         
-        self.myLabel.frame = labelFrame
-        self.myLabel.numberOfLines = 0
-        self.myLabel.lineBreakMode = .byWordWrapping
-        self.myLabel.text = "Hello, I'm under the water, help me"
-        self.myLabel.adjustsFontSizeToFitWidth = true
-        self.myLabel.sizeToFit()
+//        NotificationCenter.default.addObserver(self, selector: #selector(textFieldDidChange), name: UITextField.textDidChangeNotification, object: nil)
         
-        self.myLabel.textColor = UIColor.red
-        self.myLabel.shadowColor = UIColor.brown
-        self.myLabel.shadowOffset = CGSize(width: 2, height: 2)
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) {
+            (nc) in
+            self.view.frame.origin.y = -200
+        }
         
-        self.myLabel.font = UIFont.boldSystemFont(ofSize: 14)
-        self.myLabel.center = self.view.center
-        
-        self.view.addSubview(myLabel)
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: nil) {
+            (nc) in
+            self.view.frame.origin.y = 0.0
+        }
       
     }
 
+//    MARK: Create UI
+    func createTextField() {
+        let textFieldFrame = CGRect(x: 0, y: 0, width: 200, height: 30)
+        
+        self.myTextField = UITextField(frame: textFieldFrame)
+        self.myTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        self.myTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        self.myTextField.textAlignment = .center
+        self.myTextField.placeholder = "Test placeholder"
+        self.myTextField.center = self.view.center
+        
+        self.view.addSubview(self.myTextField)
+    }
     
-
+//    MARK: Notification
+    @objc func textFieldDidChange(ncParam: NSNotification) {
+        print("didChange = \(ncParam)")
+    }
+    
+// MARK: TextFieldDelegate
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("can I edit this field?")
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("Started editing field")
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("Can I finish editing?")
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("Finished editing ")
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("Your input \(string)")
+  
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print("You deleted some")
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("Close keyboard?")
+        if textField == myTextField{
+            self.myTextField.resignFirstResponder()
+        }
+        return true
+    }
 }
