f5be228d1233fb177ec3a957a5b1d663234c07df
Daniil Silin <daniil_silin@mail.ru>
2022-04-03 00:42:34 +0100

    Animations

--- UIKitTest/ViewController.swift
+++ UIKitTest/ViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-  
+    var squareView = [UIDynamicItem]()
+    var animator = UIDynamicAnimator()
 
     
      override func viewDidLoad() {
@@ -18,6 +19,49 @@ final class ViewController: UIViewController {
          
     }
     
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+//        squareView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//        squareView.backgroundColor = .green
+//        squareView.center = view.center
+//        view.addSubview(squareView)
+//
+//            //    Create animator
+//        animator = UIDynamicAnimator(referenceView: view)
+//        let gravity = UIGravityBehavior(items: [squareView])
+//        animator.addBehavior(gravity)
+        
+        
+        let numOfView = 2
+        squareView.reserveCapacity(numOfView)
+        let colors: [UIColor] = [.red, .green]
+        var currentCenterPoint: CGPoint = view.center
+        let eachViewSize = CGSize(width: 50, height: 50)
+        
+        for counter in 0..<numOfView {
+            let newView = UIView(frame: CGRect(x: 0, y: 0, width: eachViewSize.width, height: eachViewSize.height))
+            
+            newView.backgroundColor = colors[counter]
+            newView.center = currentCenterPoint
+            currentCenterPoint.y += eachViewSize.height + 10
+            view.addSubview(newView)
+            squareView.append(newView)
+        }
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        
+        let gravity = UIGravityBehavior(items: squareView)
+        animator.addBehavior(gravity)
+        
+        let collision = UICollisionBehavior(items: squareView)
+        collision.translatesReferenceBoundsIntoBoundary = true
+        
+        animator.addBehavior(collision)
+    }
+    
+
     
 
 }
